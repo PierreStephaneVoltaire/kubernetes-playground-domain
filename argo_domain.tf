3ddea7fe6d4483189374b5114f87383879006217
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-05-01 09:52:49 -0400

    Revert "Update argo_domain.tf"
    This reverts commit a624b2680993cd538d4891da1dbecd4053d8e875.

--- argo_domain.tf
+++ argo_domain.tf
@@ -8,17 +8,21 @@ data "aws_route53_zone" "main" {
 
 
 
-data "aws_elb" "argo" {
-  name = "a25e3f517090b4c0d9c10f0254a8928b"
+data "aws_lbs" "argo" {
+  tags = {
+    "ingress.k8s.aws/stack" = "argocd/argo-cd-argocd-server"
+  }
 }
-
+data "aws_lb" "argo" {
+  arn = one(data.aws_lbs.argo.arns)
+}
 resource "aws_route53_record" "argocd_alb" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = local.argo_domain
   type    = "A"
   alias {
-    name                   = data.aws_elb.argo.dns_name
-    zone_id                = data.aws_elb.argo.zone_id
+    name                   = data.aws_lb.argo.dns_name
+    zone_id                = data.aws_lb.argo.zone_id
     evaluate_target_health = true
   }
 }
